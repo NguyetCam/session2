3900d50b2cc40717283aaab5ab5846b53d7f3299
NguyetCam <96852028+NguyetCam@users.noreply.github.com>
2022-03-21 18:59:14 +0700

    Update fraction.java

--- fraction/fraction.java
+++ fraction/fraction.java
@@ -1,7 +1,9 @@
 package fraction;
 
+import java.util.Scanner;
+
 public class fraction {
-    int a,b;
+    int a,b,c,d;
 
     fraction(){
         System.out.println("Nhập tử số và mẫu số");
@@ -22,27 +24,116 @@ public class fraction {
         this.b = b;
     }
 
-    boolean kiemtra(){
-        if(getB()==0) return true;
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    boolean kiemtra(int ms){
+        if(ms==0) return true;
         return false;
     }
 
-    void rutgon(){
-        System.out.println("Hàm rút gọn phân số");
+    int UCLN(int a,int b){
+        int i;
+        if(a==0) return b;
+        else {
+            if(Math.abs(a)>Math.abs(b)){
+                for(i=Math.abs(b);i>=1;i--){
+                    if(a%i==0&&b%i==0) return i;
+                }
+            } else {
+                for (i=Math.abs(a);i>=1;i--) {
+                    if (a%i==0&&b%i==0) return i;
+                }
+            }
+            return i;
+        }
+    }
+    int BCNN(int a,int b){
+        int ucln = UCLN(a,b);
+        return ucln*a*b;
+    }
+    void rutgon(int a,int b){
+        int ucln = UCLN(a,b);
+        int ts=a/ucln;
+        int ms=b/ucln;
+        if(ts==0) System.out.println("Phân số rút gọn: 0");
+        if(ms==1) System.out.println("Rút gọn: " + ts);
+        else {
+            if(ts<0&&ms<0){
+                ts=Math.abs(ts);
+                ms=Math.abs(ms);
+                System.out.println("Rút gọn: " + ts + "/" + ms);
+            }
+            System.out.println("Rút gọn: " + ts + "/" + ms);
+        }
     }
     void nghichdao(){
-        System.out.println("Hàm nghịch đảo phân số");
+        if(getA()==0){
+            System.out.println("Phân số không thể nghịch đảo vì tử số cần khác 0");
+        }else {
+            System.out.println("Phân số nghịch đảo: " + getB() + "/" + getA());
+            rutgon(getB(),getA());
+        }
     }
-    void add(){
-        System.out.println("Hàm cộng phân số");
+    void pso2(){
+        System.out.println("Nhập phân số thứ hai");
+        Scanner sc = new Scanner(System.in);
+        setC(sc.nextInt());
+        setD(sc.nextInt());
+        if(kiemtra(getD())) System.out.println("Mẫu số phải khác 0");
+        else {
+            add(getA(), getB(), getC(), getD());
+            sub(getA(), getB(), getC(), getD());
+            multi(getA(), getB(), getC(), getD());
+            div(getA(), getB(), getC(), getD());
+        }
     }
-    void sub(){
-        System.out.println("Hàm trừ phân số");
+    void add(int a, int b, int c, int d){
+        int x,y,ts,ms;
+        int bcnn = BCNN(b,d);
+        x = bcnn/b;
+        y = bcnn/d;
+        ts = a*x + c*y;
+        ms = bcnn;
+        System.out.println("Tổng hai phân số: " + getA() + "/" + getB() + " + " + getC() + "/" + getD() + " = " + ts + "/" + ms);
     }
-    void multi(){
-        System.out.println("Hàm nhân phân số");
+    void sub(int a, int b, int c, int d){
+        int x,y,ts,ms;
+        int bcnn = BCNN(b,d);
+        x = bcnn/b;
+        y = bcnn/d;
+        ts = a*x - c*y;
+        ms = bcnn;
+        System.out.println("Hiệu hai phân số: " + getA() + "/" + getB() + " - " + getC() + "/" + getD() + " = " + ts + "/" + ms);
     }
-    void div(){
-        System.out.println("Hàm chia phân số");
+    void multi(int a, int b, int c, int d){
+        int ts,ms;
+        ts = a*c;
+        ms = b*d;
+        System.out.println("Tích hai phân số: " + getA() + "/" + getB() + " * " + getC() + "/" + getD() + " = " + ts + "/" + ms);
+        rutgon(ts,ms);
+    }
+    void div(int a, int b, int c, int d){
+        int ts,ms;
+        if(getC()==0) System.out.println("Số bị chia phải khác 0");
+        else {
+            ts = a*d;
+            ms = b*c;
+            System.out.println("Thương hai phân số: " + getA() + "/" + getB() + " / " + getC() + "/" + getD() + " = " + ts + "/" + ms);
+            rutgon(ts,ms);
+        }
     }
 }
